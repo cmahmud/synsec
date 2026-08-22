945fdd0379e006a6a4ab92516201ceb31f9c26ae
Mahmud Chowdhury <ciram987@gmail.com>
2026-08-22 03:48:04 -0400

    feat(scanners): prefer Betterleaks while retaining Gitleaks fallback

--- packages/scanners/src/index.ts
+++ packages/scanners/src/index.ts
@@ -1,4 +1,5 @@
 import type { ScannerAdapter } from "@synsec/scanner-sdk";
+import { BetterleaksAdapter } from "./betterleaks.js";
 import { CheckovAdapter } from "./checkov.js";
 import { GitleaksAdapter } from "./gitleaks.js";
 import { GrypeAdapter } from "./grype.js";
@@ -6,6 +7,7 @@ import { OpengrepAdapter } from "./opengrep.js";
 import { OsvScannerAdapter } from "./osv.js";
 import { TrivyAdapter } from "./trivy.js";
 
+export { BetterleaksAdapter, parseBetterleaksJson } from "./betterleaks.js";
 export { CheckovAdapter, parseCheckovJson } from "./checkov.js";
 export { GitleaksAdapter, parseGitleaksJson } from "./gitleaks.js";
 export { GrypeAdapter, parseGrypeJson } from "./grype.js";
@@ -16,6 +18,7 @@ export { TrivyAdapter, parseTrivyJson } from "./trivy.js";
 export function builtInScanners(): ScannerAdapter[] {
   return [
     new OpengrepAdapter(),
+    new BetterleaksAdapter(),
     new GitleaksAdapter(),
     new OsvScannerAdapter(),
     new TrivyAdapter(),
